dd844344ba17fda6f30d70cdbad92239e9d582a1
utkarshsingh004 <singhutkarrshkumar097@gmail.com>
2025-06-12 20:18:42 +0530

    Update Frontend & Backend for deployment

--- Backend/src/DB/server.js
+++ Backend/src/DB/server.js
@@ -4,6 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async ()=> {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log("Current working directory:", process.cwd());
         console.log(`MongoDB connected !! BD host: ${connectionInstance.connection.host}`);
         
     } catch (error) {
